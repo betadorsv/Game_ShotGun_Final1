3fc62aaa4fab5787058896dffef404ba6cba2307
betadorsv <mr.betran.qn@gmail.com>
2020-06-15 01:28:28 +0700

    first commit by Be

---
+++ ShotGun_Game_Unity/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    // Start is called before the first frame update
+
+
+    void Awake()
+    {
+     foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }   
+    }
+
+    void Start()
+    {
+        Play("Theme");
+    }
+
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            return;
+        }
+        s.source.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ShotGun_Game_Unity/Assets/Scripts/UI/PauseUI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseUI : MonoBehaviour
+{
+    public static bool GameIsPause = false;
+    public GameObject pauseUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
+     
+
+
+    }
+
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+    public void Restart()
+    {
+        GameIsPause = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
+
+
